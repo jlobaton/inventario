e577a4aa24fa8d56346eb821f845a48ea87f98e3
Jesus Lobaton <jesuslobaton@gmail.com>
2016-02-07 01:44:09 -0430

    se agregro la plantilla del correo como enviado

--- resources/views/correos/enviado.blade.php
+++ resources/views/correos/enviado.blade.php
@@ -1,12 +1,35 @@
-<p>
-  You have received a new message from your website contact form.
-</p>
-<p>
-  Here are the details:
-</p>
+<table id="Tabla_01" style="width: 630px;" border="0" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<td><a onmouseover="window.status='Seguridad y Sistema JM';  return true;" onmouseout="window.status='';  return true;" href="http://twitter.com/SyS_JM"> <img src="http://www.toyolobaimport.com/ML/GraciasPorSuCompra/Images/GraciasPorSuCompra_01.png" alt="Seguridad y Sistema JM" width="630" height="102" border="0" /></a></td>
+</tr>
+<tr>
+<td><img src="http://www.toyolobaimport.com/ML/GraciasPorSuCompra/Images/GraciasPorSuCompra_02.png" alt="" width="630" height="48" /></td>
+</tr>
+<tr>
+<td><a onmouseover="window.status='Su Pedido Ha Sido Enviado, Revisar el Archivo Adjunto';  return true;" onmouseout="window.status='';  return true;" href="#"> <img src="http://www.toyolobaimport.com/ML/GraciasPorSuCompra/Images/GraciasPorSuCompra_03.png" alt="Su Pedido Ha Sido Enviado, Revisar el Archivo Adjunto" width="630" height="76" border="0" /></a></td>
+</tr>
+<tr>
+<td>
 <ul>
-  <li>Email: <strong>{{ $correo }}</strong></li>
-  <li>nroguia: <strong>{{ $nroguia }}</strong></li>
-
+<li><strong>Destinatario:</strong> {{ $nroguia }}</li>
+<li><strong>Direccion Destino:</strong> {{ $nroguia }}</li>
+<li><strong>Servicio Encomienda:</strong> {{ $nroguia }}</li>
+<li><strong>Nro de Cupon:</strong> {{ $nroguia }}</li>
+<li><strong>Fecha de Envio:</strong> {{ $fecha }}</li>
+<li><strong>Total Cajas/Paquetes:</strong> 1</li>
 </ul>
-<hr>
+<p>&nbsp;</p>
+</td>
+</tr>
+<tr>
+<td><img src="http://www.toyolobaimport.com/ML/GraciasPorSuCompra/Images/GraciasPorSuCompra_04.png" alt="" width="630" height="90" /></td>
+</tr>
+<tr>
+<td><a onmouseover="window.status='Recuerde Calificarnos...';  return true;" onmouseout="window.status='';  return true;" href="http://www.mercadolibre.com.ve/jm/myML?as_section=MY_PURCH"> <img src="http://www.toyolobaimport.com/ML/GraciasPorSuCompra/Images/GraciasPorSuCompra_05.png" alt="Recuerde Calificarnos..." width="630" height="119" border="0" /></a></td>
+</tr>
+<tr>
+<td><a onmouseover="window.status='Visitenos en Facebook y Twitter';  return true;" onmouseout="window.status='';  return true;" href="#"> <img src="http://www.toyolobaimport.com/ML/GraciasPorSuCompra/Images/GraciasPorSuCompra_06.png" alt="Visitenos en Facebook y Twitter" width="630" height="65" border="0" /></a></td>
+</tr>
+</tbody>
+</table>
